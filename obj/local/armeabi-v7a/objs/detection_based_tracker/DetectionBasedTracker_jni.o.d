c2138c6c939e3143ae2e56673aea73cc06afb170
Waqas <wikkii@msn.com>
2015-08-16 06:22:59 -0400

    3

---
+++ obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,148 @@
+obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/jni.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/sys/cdefs.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/android/api-level.h \
+ ../../sdk/native/jni/include/opencv2/core/core.hpp \
+ ../../sdk/native/jni/include/opencv2/core/types_c.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/assert.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/stdlib.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/string.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/malloc.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/xlocale.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/alloca.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/strings.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/sys/types.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/stdint.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/machine/wchar_limits.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/linux/types.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/asm/types.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/asm-generic/types.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/asm-generic/int-ll64.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/asm/bitsperlong.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/asm-generic/bitsperlong.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/linux/posix_types.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/linux/stddef.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/linux/compiler.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/asm/posix_types.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/asm-generic/posix_types.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/sys/sysmacros.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/memory.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/math.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/limits.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/sys/limits.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/linux/limits.h \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/sys/syslimits.h \
+ ../../sdk/native/jni/include/opencv2/core/version.hpp \
+ C:/android-ndk/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+ C:/android-ndk/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath \
+ C:/android-ndk/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
+ C:/android-ndk/sources/cxx-stl/gnu-libstdc++/4.9/include/complex \
+ C:/android-ndk/sources/cxx-stl/gnu-libstdc++/4.9/include/map \
+ C:/android-ndk/sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+ ../../sdk/native/jni/include/opencv2/core/operations.hpp \
+ C:/android-ndk/sources/cxx-stl/gnu-libstdc++/4.9/include/limits \
+ ../../sdk/native/jni/include/opencv2/core/mat.hpp \
+ ../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ ../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ C:/android-ndk/sources/cxx-stl/gnu-libstdc++/4.9/include/deque \
+ C:/android-ndk/platforms/android-L/arch-arm/usr/include/android/log.h
+
+jni/DetectionBasedTracker_jni.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/jni.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/sys/cdefs.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/android/api-level.h:
+
+../../sdk/native/jni/include/opencv2/core/core.hpp:
+
+../../sdk/native/jni/include/opencv2/core/types_c.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/assert.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/stdlib.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/string.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/malloc.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/xlocale.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/alloca.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/strings.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/sys/types.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/stdint.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/machine/wchar_limits.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/linux/types.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/asm/types.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/asm-generic/types.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/asm-generic/int-ll64.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/asm/bitsperlong.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/asm-generic/bitsperlong.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/linux/posix_types.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/linux/stddef.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/linux/compiler.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/asm/posix_types.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/asm-generic/posix_types.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/memory.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/math.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/limits.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/sys/limits.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/linux/limits.h:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/sys/syslimits.h:
+
+../../sdk/native/jni/include/opencv2/core/version.hpp:
+
+C:/android-ndk/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+C:/android-ndk/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath:
+
+C:/android-ndk/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef:
+
+C:/android-ndk/sources/cxx-stl/gnu-libstdc++/4.9/include/complex:
+
+C:/android-ndk/sources/cxx-stl/gnu-libstdc++/4.9/include/map:
+
+C:/android-ndk/sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+../../sdk/native/jni/include/opencv2/core/operations.hpp:
+
+C:/android-ndk/sources/cxx-stl/gnu-libstdc++/4.9/include/limits:
+
+../../sdk/native/jni/include/opencv2/core/mat.hpp:
+
+../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+C:/android-ndk/sources/cxx-stl/gnu-libstdc++/4.9/include/deque:
+
+C:/android-ndk/platforms/android-L/arch-arm/usr/include/android/log.h:
